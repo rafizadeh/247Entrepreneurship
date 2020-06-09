8bb2afc47810fa375bc706d76dd363643f19c33e
rafizadeh <42311158+rafizadeh@users.noreply.github.com>
2020-06-09 09:46:33 +0400

    js fixed

--- js/script.js
+++ js/script.js
@@ -1,11 +1,8 @@
 let btn = document.querySelector('.navbar-toggler');
-console.log(btn);
 
 let menu = document.querySelector('.navbar-collapse');
-console.log(menu);
 
 btn.addEventListener('click' , e=>{
-    console.log('a');
     
     if(menu.classList.contains('collapse')){
         menu.classList.remove('collapse')
